479c3d45ed5910c54d60c564858dfdf7a2275099
Jared Johnson <89032304+JJohnson-DA@users.noreply.github.com>
2023-12-01 09:30:56 -0600

    Day 1

--- src/d01.py
+++ src/d01.py
@@ -20,20 +20,27 @@ words = [
 
 with open("../data/d01.txt", "r") as f:
     for line in f:
-        p1 = []
+        # digits = [match.group() for match in re.finditer(pattern, line)]
+        # p1_temp = []
+        # p2_temp = []
+        # for digit in digits:
+        #     if digit.isdigit():
+        #         p1_temp.append(digit)
+        #         p2_temp.append(digit)
+        #     else:
+        #         p2_temp.append(str(words.index(digit) + 1))
+        # p1_digits.append("".join(p1_temp))
+        # p2_digits.append("".join(p2_temp))
+        p1, p2 = [], []
         for i, d in enumerate(line):
             if d.isdigit():
                 p1.append(d)
-        p1_digits.append("".join(p1))
-
-        p2 = []
-        for i, d in enumerate(line):
-            if d.isdigit():
                 p2.append(d)
             else:
                 for j, word in enumerate(words):
                     if line[i:].startswith(word):
                         p2.append(str(j + 1))
+        p1_digits.append("".join(p1))
         p2_digits.append("".join(p2))
 
 
