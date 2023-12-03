95eebca8d0fd39143b33232b34acfdc8952c9197
Jared-Johnson <jaj53223@bethel.edu>
2023-12-03 15:09:22 -0600

    redo part 1 with part 2 strat

--- src/d03.py
+++ src/d03.py
@@ -1,17 +1,8 @@
 # %%
-
 import re
 from playground import *
 
 lines = load_input_lines(__file__)
-
-
-def contains_symbol(string):
-    for char in string:
-        if not char.isdigit() and char != ".":
-            return True
-
-    return False
 
 
 def match_indices(substring, string):
@@ -21,27 +12,7 @@ def match_indices(substring, string):
         return [int(i) for i in range(match.start(), match.end())]
 
 
-def extract_perimiter(indices, i, lines):
-    max_len = len(lines[0]) - 1
-    start_c = max(indices[0] - 1, 0)
-    end_c = min(indices[-1] + 2, max_len)
-
-    start_r = max(i - 1, 0)
-    end_r = min(i + 2, len(lines))
-
-    chars = ""
-
-    for column in range(start_c, end_c):
-        for row in range(start_r, end_r):
-            chars += lines[row][column]
-
-    return "".join(x if not x.isdigit() else "" for x in chars)
-
-
-p1 = 0
-p2 = 0
-
-num_map = {}
+p1, p2, num_map, symbol_map = 0, 0, {}, {}
 
 for i, line in enumerate(lines):
     num_map[i] = {}
@@ -49,15 +20,31 @@ for i, line in enumerate(lines):
         for j in range(match.start(), match.end()):
             num_map[i][j] = match.group()
 
+    symbol_map[i] = {}
+    for j, c in enumerate(line):
+        if not c.isdigit() and c != ".":
+            symbol_map[i][j] = c
+
+
 for i, line in enumerate(lines):
     # Part 1
     numbers = re.findall("\d+", line)
     for number in numbers:
         indices = match_indices(number, line)
-        if indices:
-            perimiter = extract_perimiter(indices, i, lines)
-            if contains_symbol(perimiter):
-                p1 += int(number)
+        number_complete = False
+        matches = []
+        for j in indices:
+            options = list(
+                set([(i + y, j + x) for y in [-1, 0, 1] for x in [-1, 0, 1]])
+            )
+            for option in options:
+                level_1 = symbol_map.get(option[0])
+                if level_1:
+                    level_2 = level_1.get(option[1])
+                    if level_2:
+                        matches.append(level_2)
+        if len(matches) > 0:
+            p1 += int(number)
 
     # Part 2
     for j, char in enumerate(line):
