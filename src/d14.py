fa8f7be5ab4bf9a8578a023b8acdf39349588e61
Jared Johnson <89032304+JJohnson-DA@users.noreply.github.com>
2023-12-14 09:47:19 -0600

    refactor

--- src/d14.py
+++ src/d14.py
@@ -4,48 +4,72 @@ from collections import Counter
 from functools import cache
 
 lines = load_lines("test")
-lines = load_lines(__file__)
+# lines = load_lines(__file__)
 lines = [list(x) for x in lines]
 
 
-all_coords = {
-    (i, j): lines[i][j] for i in range(len(lines)) for j in range(len(lines[0]))
-}
+class Reflector:
+    def __init__(self, lines) -> None:
+        self.grid = [list(x) for x in lines]
+        self.roll_cache = {}
 
-ds = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+    # @cache
+    def rotate_grid(self):
+        r, c = len(self.grid), len(self.grid[0])
+        g = [[self.grid[i][j] for i in range(r - 1, -1, -1)] for j in range(c)]
+        return g
 
-grid_cache = {}
+    # @cache
+    def roll_rocks(self, g):
+        gt = tuple(tuple(gc) for gc in g)
+        if self.roll_cache.get(gt):
+            return self.roll_cache.get(gt)
+        else:
+            gn = g.copy()
+            coords = {(i, j): g[i][j] for i in range(len(g)) for j in range(len(g[0]))}
+            rocks_rolled = 1
+            while rocks_rolled > 0:
+                rocks_rolled = 0
+                for k, v in coords.items():
+                    if v == "O":
+                        nv = coords.get((k[0] - 1, k[1]))
+                        if nv == ".":
+                            coords[((k[0] - 1, k[1]))] = "O"
+                            coords[((k[0], k[1]))] = "."
+                            rocks_rolled += 1
 
-# for i in range(1000000000):
-# for d in ds:
-#     grid_start = tuple([tuple(x) for x in lines.copy()])
+            for k, v in coords.items():
+                gn[k[0]][k[1]] = v
 
-#     if (d, grid_start) in grid_cache.keys():
-#         lines = grid_cache[(d, grid_start)]
-# else:
-for d in ds:
-    rocks_rolled = 1
-    while rocks_rolled > 0:
-        # Reset for look
-        rocks_rolled = 0
-        for k, v in all_coords.items():
-            if v == "O":
-                # check item above it, and get coords. if it is empty, roll rock up
-                nv = all_coords.get((k[0] + d[0], k[1] + d[1]))
-                if nv == ".":
-                    all_coords[((k[0] + d[0], k[1] + d[1]))] = "O"
-                    all_coords[((k[0], k[1]))] = "."
-                    # add to roll count
-                    rocks_rolled += 1
+            self.roll_cache[gt] = gn
 
-    for k, v in all_coords.items():
-        lines[k[0]][k[1]] = v
+            return gn
 
-    break
+    def cycle(self):
+        for i in range(4):
+            self.roll_rocks(self.grid)
+            self.rotate_grid()
 
-    # grid_cache[(d, grid_start)] = lines
+    def score_grid(self):
+        p1 = 0
+        for i, line in enumerate(self.grid[::-1]):
+            c = Counter(line)
+            p1 += (i + 1) * c["O"]
+        return p1
 
-p1 = 0
-for i, line in enumerate(lines[::-1]):
-    c = Counter(line)
-    p1 += (i + 1) * c["O"]
+    def part_one(self):
+        self.roll_rocks(self.grid)
+        print(f"Part 1: {self.score_grid()}")
+
+    def part_two(self):
+        cycles_target = 1000000000
+        cycles_run = 0
+        while cycles_run < cycles_target:
+            cycles_run += 1
+            self.cycle()
+
+        print(f"Part 2: {self.score_grid()}")
+
+
+R = Reflector(lines)
+R.part_one()
